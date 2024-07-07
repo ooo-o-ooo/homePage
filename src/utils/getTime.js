528e46facdbb53aa65bda5a6f014f92ddd5ec619
Ross <wanglu2325@qq.com>
2024-07-07 22:23:37 +0800

    修改

--- src/utils/getTime.js
+++ src/utils/getTime.js
@@ -100,11 +100,6 @@ export const helloInit = () => {
 
 // 默哀模式
 const anniversaries = {
-  4.4: "清明节",
-  5.12: "汶川大地震纪念日",
-  7.7: "中国人民抗日战争纪念日",
-  9.18: "九·一八事变纪念日",
-  12.13: "南京大屠杀死难者国家公祭日",
 };
 export const checkDays = () => {
   const myDate = new Date();
